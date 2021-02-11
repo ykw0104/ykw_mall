10092f22df2a1e5828f7abab71bde7b70d768706
D.Boy.Blade <ykw0104@163.com>
2021-02-11 15:07:14 +0800

    TabControl

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" v-for="(item,index) of titles" :key="item" :class="{active:index == currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  components: {},
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+  ,
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  font-size: 14px;
+  height: 40px;
+  line-height: 40px;
+}
+.tab-control-item {
+  flex: 1;
+}
+
+.tab-control-item span {
+  padding: 5px;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="feature">
+    <a href="">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureView',
+  components: {},
+  data() {
+    return {
+    }
+  },
+}
+</script>
+<style scoped>
+.feature img {
+  width: 100%;
+}
+</style>
