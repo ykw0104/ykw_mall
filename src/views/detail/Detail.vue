12cc2f13ce558a5e99fe1e32bc1cd11a7c517026
D.Boy.Blade <ykw0104@163.com>
2021-02-20 15:23:02 +0800

    p180 跳转到详情页并携带iid

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{iid}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Detail',
+  components: {},
+  data() {
+    return {
+      iid: null
+    }
+  },
+  created() {
+    this.iid = this.$route.params.iid
+  },
+}
+</script>
+<style scoped>
+</style>
